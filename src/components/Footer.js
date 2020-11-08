efd9538f9b673def387011a94519f29d8cb8d568
Yaanibaano <chandru.vijayan101999@gmail.com>
2020-11-08 14:49:44 +0530

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,78 @@
+import React from "react";
+import "../App.css"
+
+var sectionStyle = {
+    width: "100%",
+    height: "200px",
+    backgroundImage: `url(${"/images/bg-boost-desktop.svg"})`
+};
+  
+
+export const PreFooter = () =>{
+    return(
+        <div className="row">
+        <div className="pre-footer-bg" style={sectionStyle}>
+        <div className="content">
+            <h1>Boost your links today</h1>
+            <div className="my-nav-items blue-oval mt-4">
+                <a href="/">Get Started</a>
+            </div>
+        </div>
+        </div>
+        </div>
+    );
+}
+
+export const Footer = () =>{
+    return(
+        <div>
+        <PreFooter />
+        <div className="footer row justify-content-center">
+            <div className = "row justify-content-between  container">
+                <div className="col-12 col-lg-2 row justify-content-center m-lg-0 ml-2">
+                    <a href="/" className="footer-brand  mr-3">Shortly</a>
+                </div>
+                <div className="col-12 row col-lg-7 justify-content-end mt-2">
+                <div className="col-12 col-lg-3 row justify-content-center mr-lg-2 mt-lg-0 mt-5">
+                        <div>
+                        <p className="list-header text-lg-left text-center">Features</p>
+                            <ul>
+                                <li><a href="/" >Link Shortening</a></li>
+                                <li><a href="/" >Branded Links</a></li>
+                                <li><a href="/">Analytics</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-3 row justify-content-center mt-lg-0 mt-4">
+                        <div>
+                        <p className="list-header">Resources</p>
+                            <ul>
+                                <li><a href="/">Blog</a></li>
+                                <li><a href="/">Developers</a></li>
+                                <li><a href="/">Support</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-3 row justify-content-center mt-lg-0 mt-4">
+                        <div>
+                            <p className="list-header">Company</p>
+                            <ul>
+                                <li><a href="/">About</a></li>
+                                <li><a href="/">Our Team</a></li>
+                                <li><a href="/">Careers</a></li>
+                                <li><a href="/">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-3 row mt-lg-0 mt-5 justify-content-between">
+                    <img src="/images/icon-facebook.svg"></img>
+                    <img src="/images/icon-instagram.svg"></img>
+                    <img src="/images/icon-pinterest.svg"></img>
+                    <img src="/images/icon-twitter.svg"></img>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+}
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,97 +1,36 @@
 import React, { Component } from "react";
+import MyNavBar from "./NavBar"
+import {Hero} from "./HeroPage"
+import {PreFooter,Footer} from "./Footer"
 import "../App.css"
 
-export const TestComponent = () => {
+export const MainC = () => {
     return (
         <h1>Hellow world</h1>
     );
 }
 
-export const collapsedItems = (isOpen)=>{
-    if(isOpen){
-    return(
-        <React.Fragment>
-        <div class="custom-card">
-            <div className="row justify-content-center my-nav-items-sm">
-                <a href="/" className="my-nav-items mt-3 mb-4">Features</a>
-            </div>
-            <div className="row justify-content-center my-nav-items-sm">
-                <a href="/" className="mb-4">Pricing</a>
-            </div>
-            <div className="row justify-content-center my-nav-items-sm">
-                <a href="/" className="mb-4">Resources</a>
-            </div>
-            <div className="row justify-content-center mb-4 ">
-                <div className="divider"></div>
-            </div>
-            <div className="row justify-content-center mb-4 my-nav-items-sm">
-                <a href="/" >Login</a>
-            </div>
-            <div className="row justify-content-center my-nav-items-sm">
-                <a href="/" className="blue-oval col-10 mb-4">Sign Up</a>
-            </div>
-        </div>
-        </React.Fragment>
-    );}
-    else{
-        <div></div>
-    }
-}  
-
-class MyNavBar extends Component {
-
-    constructor(props){
-        super(props)
-        this.state = {
-            isOpen:false
-        }
-        this.toggleNav = this.toggleNav.bind(this)
-    }
-
-    toggleNav(){
-        this.setState({isOpen:!this.state.isOpen})
-        console.log(this.state.isOpen)
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="row bg-red justify-lg-content-center justify-content-between">
-                    <div className="col-2 row justify-content-center bg-green text-center ml-5 ml-lg-0 ml-sm-3">
-                        <a className="brand" href="/">Shortly</a>
-                    </div>
-                    <div className="col-7 row bg-yellow ml-3 d-lg-flex d-none">
-                        <div className="col-2 my-nav-items">
-                            <a href="/">Features</a>
-                        </div>
-                        <div className="col-2 my-nav-items">
-                            <a href="/">Pricing</a>
-                        </div>
-                        <div className="col-2 my-nav-items">
-                            <a href="/">Resources</a>
-                        </div>
-                    </div>
-                    <div className="col-3 row bg-green d-lg-flex d-none">
-                        <div className="col-5 my-nav-items">
-                            <a href="/">Log-in</a>
-                        </div>
-                        <div className="col-6 my-nav-items blue-oval">
-                            <a href="/">Sign Up</a>
-                        </div>
-                    </div>
-                    <div className="col-3 row d-lg-none justify-content-center ml-3">
-                        <span className="fa fa-bars fa-2x" onClick={this.toggleNav}></span>
-                    </div>
-                </div>
-                <div className="d-lg-none">
-                    {collapsedItems(this.state.isOpen)}
-                </div>
-            </div>
-        )
+class MainComponent extends Component{
+    render(){
+        return(
+            <React.Fragment>
+                <MyNavBar></MyNavBar>
+                <Hero></Hero>
+                
+                <Footer></Footer>
+                
+            </React.Fragment>
+        );
     }
 }
 
-export default MyNavBar
+export default MainComponent
+
+ 
+
+
+
+
 
 
 
